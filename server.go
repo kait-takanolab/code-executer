6271b84c16dac2eb154ae641a84957eeb08ae4ff
URANO Ryoya <urano.works.mail@gmail.com>
2018-05-24 19:03:23 +0900

    dockerでソースコードを実行するように変更

--- server.go
+++ server.go
@@ -8,19 +8,28 @@ import (
 	"io/ioutil"
 	"net/http"
 	"time"
+	"os"
 
 	"github.com/kait-takanolab/code-executer/sandbox"
+	"github.com/docker/docker/client"
+	"golang.org/x/net/context"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types"
+	"archive/tar"
+	"github.com/pkg/errors"
 )
 
 type server struct {
+	docker *client.Client
+	dockerCtx context.Context
 	mux *http.ServeMux
 }
 
-type Req struct {
+type ReqJson struct {
 	Code string `json:"code"`
 }
 
-type Rsp struct {
+type RspJson struct {
 	Status     string        `json:"status"`
 	Stdout     string        `json:"stdout"`
 	Stderr     string        `json:"stderr"`
@@ -28,15 +37,60 @@ type Rsp struct {
 	SystemTime time.Duration `json:"system_time"`
 }
 
+func panicResponse(w http.ResponseWriter, errMsg string) {
+	rsp, _ := json.Marshal(RspJson{Status: errMsg})
+	w.Write(rsp)
+}
+
+type SourceCodeFile struct {
+	Name, Body string
+}
+
+func tarMaker(files []SourceCodeFile) (*bytes.Buffer, error) {
+	var buf bytes.Buffer
+	tw := tar.NewWriter(&buf)
+	for _, file := range files {
+		header := &tar.Header{
+			Name: file.Name,
+			Mode: 0600,
+			Size: int64(len(file.Body)),
+		}
+		if err := tw.WriteHeader(header); err != nil {
+			return nil, errors.New("tw.WriteHeader({" + header.Name + "}): " + err.Error())
+		}
+		if _, err := tw.Write([]byte(file.Body)); err != nil {
+			return nil, errors.New("tw.Write({" + header.Name + "}): " + err.Error())
+		}
+	}
+	if err := tw.Close(); err != nil {
+		return nil, errors.New("tar writer close failed.")
+	}
+	return &buf, nil
+}
+
 func newServer() (*server, error) {
-	s := &server{mux: http.NewServeMux()}
+	ctx := context.Background()
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		return nil, err // write alternative error
+	}
+	reader, err := cli.ImagePull(ctx, "docker.io/library/golang", types.ImagePullOptions{})
+	if err != nil {
+		return nil, err // write alternative error
+	}
+	io.Copy(os.Stdout, reader)
+	s := &server{
+		docker: cli,
+		dockerCtx: ctx,
+		mux: http.NewServeMux(),
+	}
 	s.init()
 	return s, nil
 }
 
 func (s *server) init() {
 	s.mux.HandleFunc("/", s.handleIndex)
-	s.mux.HandleFunc("/compile", s.handleCompile)
+	s.mux.HandleFunc("/compile", s.handleDockerCompile)
 
 	staticHandler := http.StripPrefix("/assets/", http.FileServer(http.Dir("./www/assets/")))
 	s.mux.Handle("/assets/", staticHandler)
@@ -46,15 +100,16 @@ func (s *server) handleIndex(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./www/index.html")
 }
 
+// 自作のSandbox環境を使ってコンパイルと実行をする
 func (s *server) handleCompile(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	// json decode
 	buf := new(bytes.Buffer)
 	io.Copy(buf, r.Body)
-	req := Req{}
+	req := ReqJson{}
 	json.Unmarshal(buf.Bytes(), &req)
 
-	// compile
+	// compile and run
 	sb, _ := sandbox.Init()
 	defer sb.Close()
 	sb.AddData("main.go", []byte(req.Code))
@@ -62,7 +117,7 @@ func (s *server) handleCompile(w http.ResponseWriter, r *http.Request) {
 	stdout, _ := cmd.StdoutPipe()
 	stderr, _ := cmd.StderrPipe()
 	if err := cmd.Start(); err != nil {
-		rsp, _ := json.Marshal(Rsp{Status: "pipe stdout failed"})
+		rsp, _ := json.Marshal(RspJson{Status: "pipe stdout failed"})
 		w.Write(rsp)
 		return
 	}
@@ -73,15 +128,82 @@ func (s *server) handleCompile(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		status = "cmd wait failed"
 	}
-	rsp := Rsp{
+	rspJson := RspJson{
 		Status:     status,
 		Stdout:     string(stdoutBytes),
 		Stderr:     string(stderrBytes),
 		UserTime:   cmd.ProcessState.UserTime(), // cmd.ProcessState はコマンド実行後に有効になる
 		SystemTime: cmd.ProcessState.SystemTime(),
 	}
-	rspJson, _ := json.Marshal(rsp)
-	w.Write(rspJson)
+	rsp, _ := json.Marshal(rspJson)
+	w.Write(rsp)
+}
+
+// Dockerを使ったSandbox環境を使ってコンパイルと実行をする
+func (s *server) handleDockerCompile(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	// json decode
+	buf := new(bytes.Buffer)
+	io.Copy(buf, r.Body)
+	req := ReqJson{}
+	json.Unmarshal(buf.Bytes(), &req)
+
+	// compile and run
+	containerConf := container.Config{
+		Image: "docker.io/library/golang",
+		WorkingDir: "/app",
+		Cmd:  []string{"go", "run", "main.go"},
+		Tty: true,
+	}
+	runRsp, err := s.docker.ContainerCreate(s.dockerCtx, &containerConf, nil, nil, "")
+	if err != nil {
+		rsp, _ := json.Marshal(RspJson{Status: err.Error()})
+		w.Write(rsp)
+		return
+	}
+
+	// I want to add file to the docker image.
+	srcCode, err := tarMaker([]SourceCodeFile{{"main.go", req.Code}})
+	if err != nil {
+		panicResponse(w, "make tar: " + err.Error())
+		return
+	}
+	if err := s.docker.CopyToContainer(s.dockerCtx, runRsp.ID, containerConf.WorkingDir, srcCode,
+		types.CopyToContainerOptions{}); err != nil {
+		if err != nil {
+			panicResponse(w, "copy to container: " + err.Error())
+			return
+		}
+	}
+
+
+	if err := s.docker.ContainerStart(s.dockerCtx, runRsp.ID, types.ContainerStartOptions{}); err != nil {
+		if err != nil {
+			panicResponse(w, "container start: " + err.Error())
+			return
+		}
+	}
+
+	statusCh, errCh := s.docker.ContainerWait(s.dockerCtx, runRsp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			panicResponse(w, "container wait: " + err.Error())
+			return
+		}
+		case <-statusCh:
+	}
+
+	out, err := s.docker.ContainerLogs(s.dockerCtx, runRsp.ID,
+		types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true})
+	if err != nil {
+		panicResponse(w, "container log: " + err.Error())
+		return
+	}
+	stdoutBytes, _ := ioutil.ReadAll(out)
+	rspJson := RspJson{Status: "ok", Stdout: string(stdoutBytes)}
+	rsp, _ := json.Marshal(rspJson)
+	w.Write(rsp)
 }
 
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
